d380e08871a7361eb886585935fa18f1afa9ca92
Pranav Nerkar <pnerkar@plex.com>
2022-01-18 22:46:29 +0530

    Created basic dashboard

--- StreamAnalytics.App/web-app/src/App.tsx
+++ StreamAnalytics.App/web-app/src/App.tsx
@@ -3,30 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { EventLog } from "./main/event-log/components";
+import CssBaseline from "@mui/material/CssBaseline";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/eventlog" element={<EventLog />}></Route>
-      </Routes>
-
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/eventlog">Event Log</Link>
-        </header>
+        <Dashboard />
+        {/* <Routes>
+          <Route path="/eventlog" element={<EventLog />}></Route>
+        </Routes>
+        <Link to="/eventlog">Event Log</Link> */}
       </div>
     </>
   );
