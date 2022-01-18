94e5f701f61d7ef21ef6f34d2bbc0d4d2743fb6a
Pranav Nerkar <pnerkar@plex.com>
2022-01-18 15:32:52 +0530

    Created app pages. Added react-router and redux.

--- StreamAnalytics.App/web-app/src/App.tsx
+++ StreamAnalytics.App/web-app/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { EventLog } from "./main/event-log/components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/eventlog" element={<EventLog />}></Route>
+      </Routes>
+
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Link to="/eventlog">Event Log</Link>
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ StreamAnalytics.App/web-app/src/main/event-log/components/EventLog.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class EventLogComponent extends React.Component {
+    render(){
+        return <div>This is event log page.</div>
+    }
+}
+
+export const EventLog = EventLogComponent;
